17c82a85159dde77e6e36f4d18bb0792b5f53d9c
Fergus Lai <ferguslai724@gmail.com>
2023-10-23 14:00:16 +0100

    Implemented Showing Different Pages of Auth and Unauthed

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthPage extends StatelessWidget {
   AuthPage({super.key});
 
-  bool authed = true;
-
   @override
   Widget build(BuildContext context) {
-    return authed
-        ? const Scaffold(body: (Text("Auth Page")))
-        : const Scaffold(body: (Text("Unauthed")));
+    return Scaffold(
+        body: StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Center(child: Text("Authed"));
+        } else {
+          return Center(child: Text("Unauthed"));
+        }
+      },
+    ));
   }
 }
